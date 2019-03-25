e69ac770d2f6e24ccfe07f864a961628e1643c0f
Diego Lopez <lopdie101@gmail.com>
2019-03-25 23:48:55 +0000

    added docs folder

---
+++ docs/script.js
@@ -0,0 +1,10 @@
+window.onload = () => {
+	video = document.querySelector('video');
+	navigator.mediaDevides.getUserMedia({
+		video: {
+			facingMode: "user"
+		}
+	}).then(stream => video.srcObject = stream)
+	.catch(error => alert("Error while trying to connect to camera"));
+
+}
