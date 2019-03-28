743d55d1266338b885a1e4fdc9faff8ba87ccab9
Diego Lopez <lopdie101@gmail.com>
2019-03-28 01:24:59 -0400

    made small modifications or something I don't remember

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,12 @@
 const IMAGE_SIZE = 256;
+
 class App {
   constructor() {
+    this.model_ready = false;
+    this.prediction_ready = false;
+    this.predicting = false;
+    this.model = {};
+    this.loadModel()
     this.inputVideo = document.querySelector('video');
     this.inputCanvas = document.querySelector('#inputCanvas');
     this.inputCanvas.width = IMAGE_SIZE;
@@ -18,14 +24,30 @@ class App {
 
     console.log(this.inputVideo);
   }
-  
+
+  async loadModel() {
+    this.model = await tf.loadLayersModel('/tfjs_model/model.json');
+    this.model_ready = true;
+  }
+
+  async predict() {
+    let input = tf.browser.fromPixels(this.inputCanvas, 3);
+    // input.toFloat().print();
+    let output = await this.model.predict(tf.reshape(input.toFloat(), [1, IMAGE_SIZE, IMAGE_SIZE, 3]));
+  }
+
   drawInputVideo() {
     let videoWidth = this.inputVideo.videoWidth;
     let videoHeight = this.inputVideo.videoHeight;
     let minVideoLength = Math.min(videoWidth, videoHeight)
+    
+    if (!this.predicting && this.model_ready) {
+      this.predicting = true;
+      this.predict();
+    }
 
     // Top left pixel of source video we wish to draw
-    let sx = 0
+    let sx = 0;
     let sy = 0; 
 
     if (videoWidth > videoHeight) {
@@ -33,12 +55,9 @@ class App {
     } else {
       sy = (videoHeight - videoWidth)/2;
     }
-    this.inputCtx.drawImage(this.inputVideo, sx, sy, minVideoLength, minVideoLength, 0, 0, IMAGE_SIZE, IMAGE_SIZE); //this.inputVideo.videoWidth, this.inputVideo.videoHeight);
-    t += 1;
+    this.inputCtx.drawImage(this.inputVideo, sx, sy, minVideoLength, minVideoLength, 0, 0, IMAGE_SIZE, IMAGE_SIZE);
     window.requestAnimationFrame(() => this.drawInputVideo());
   }
 }
-let t = 1
-
 
 window.onload = () => {window.app = new App()};
