c7c5b0b653833279dae9ec88d3be15202ecc261d
Diego Lopez <lopdie101@gmail.com>
2019-03-28 02:44:22 -0400

    basically done

--- docs/script.js
+++ docs/script.js
@@ -12,6 +12,9 @@ class App {
     this.inputCanvas.width = IMAGE_SIZE;
     this.inputCanvas.height = IMAGE_SIZE;
     this.inputCtx = this.inputCanvas.getContext('2d');
+    this.outputCanvas = document.querySelector('#outputCanvas');
+    this.outputCanvas.width = IMAGE_SIZE;
+    this.outputCanvas.height = IMAGE_SIZE;
 
     navigator.mediaDevices.getUserMedia({
       video: {
@@ -26,21 +29,37 @@ class App {
   }
 
   async loadModel() {
-    this.model = await tf.loadLayersModel('/tfjs_model/model.json');
+    this.model = await tf.loadLayersModel('/fast_tfjs_model/model.json');
     this.model_ready = true;
   }
 
   async predict() {
-    let input = tf.browser.fromPixels(this.inputCanvas, 3);
-    // input.toFloat().print();
-    let output = await this.model.predict(tf.reshape(input.toFloat(), [1, IMAGE_SIZE, IMAGE_SIZE, 3]));
+    let red, green, blue;
+    let colour_channel_shape = [IMAGE_SIZE, IMAGE_SIZE, 1];
+
+    let input = tf.browser.fromPixels(this.inputCanvas, 3).asType('float32');
+
+    red = tf.add(input.slice([0, 0, 0], colour_channel_shape), tf.fill(colour_channel_shape, -103.939));
+    green = tf.add(input.slice([0, 0, 1], colour_channel_shape), tf.fill(colour_channel_shape, -116.779));
+    blue = tf.add(input.slice([0, 0, 2], colour_channel_shape), tf.fill(colour_channel_shape, -123.68));
+    input = tf.concat([red, green, blue], 2)
+
+    let output = await this.model.predict(input.expandDims(0)); // tf.reshape(input), [1, IMAGE_SIZE, IMAGE_SIZE, 3]));
+    output = output.squeeze(0);
+    red = tf.add(output.slice([0, 0, 0], colour_channel_shape), tf.fill(colour_channel_shape, 103.939));
+    green = tf.add(output.slice([0, 0, 1], colour_channel_shape), tf.fill(colour_channel_shape, 116.779));
+    blue = tf.add(output.slice([0, 0, 2], colour_channel_shape), tf.fill(colour_channel_shape, 123.68));
+    output = tf.concat([red, green, blue], 2).asType('int32');
+    output = tf.clipByValue(output, 0, 255);
+    tf.browser.toPixels(output, this.outputCanvas);
+    this.predicting = false;
   }
 
   drawInputVideo() {
     let videoWidth = this.inputVideo.videoWidth;
     let videoHeight = this.inputVideo.videoHeight;
     let minVideoLength = Math.min(videoWidth, videoHeight)
-    
+
     if (!this.predicting && this.model_ready) {
       this.predicting = true;
       this.predict();
